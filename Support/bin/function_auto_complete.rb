554ad54a8cdfddc89de61ff9ce8b5415e48ac650
Dr Nic Williams <drnicwilliams@gmail.com>
2008-08-01 00:09:14 +1000

    using sexier menu for selections

--- Support/bin/function_auto_complete.rb
+++ Support/bin/function_auto_complete.rb
@@ -8,16 +8,19 @@ require "ui"
 class FunctionAutoComplete
   def self.run(partial_word)
     return partial_word unless function_list = FunctionDefn.find_by_partial(partial_word)
-    return partial_word unless function_name = TextMate::UI.request_item(:title => "Select function", :items => function_list.map { |func_name| func_name })
-
+    function_name = function_list.first
+    if function_list.length > 1
+      functions = function_list.map { |func_name| {"title" => func_name} }
+      return partial_word unless function_name = TextMate::UI.menu(functions)
+      function_name = function_name["title"]
+    end
     function_interfaces_hash = FunctionDefn.find_by_name(function_name)
     function_interfaces = function_interfaces_hash.values.sort { |a, b| a.interface <=> b.interface }
     function = function_interfaces.first
     if function_interfaces.length > 1
-      signatures = function_interfaces.map { |f| f.signature }
-      signature = TextMate::UI.request_item(:title => "Select interface", :items => signatures)
-      return partial_word unless signature
-      function = function_interfaces.find { |interface| interface.signature == signature }
+      signatures = function_interfaces.map { |f| {"title" => f.signature} }
+      return partial_word unless signature = TextMate::UI.menu(signatures)
+      function = function_interfaces.find { |interface| interface.signature == signature["title"] }
     end
 
     # generate the snippet
