f4d48dc9d01fa0fffd72a97b323467bb7aaf2ddc
Dr Nic Williams <drnicwilliams@gmail.com>
2008-08-06 23:15:54 +1000

    added google analytics tracking code

--- wp-content/themes/rounded-v2-blue-edition-10/footer.php
+++ wp-content/themes/rounded-v2-blue-edition-10/footer.php
@@ -63,6 +63,13 @@
 
 <?php credits(); do_action('wp_footer'); ?>
 
-
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-567811-12");
+pageTracker._trackPageview();
+</script>
 </body>
 </html>
